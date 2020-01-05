c33232295f0eacb465fba157b2a1f7083d073dbf
kerwei <kerwei84@gmail.com>
2020-01-05 19:24:41 +0800

    Add a new Genre entity. Also add a cross ref associative table for Book and Genre

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, Integer, Unicode, Date, DateTime
+from sqlalchemy import Column, Integer, Unicode, Date, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
@@ -12,7 +12,7 @@ class Book(Base):
     '''
     Schema of books
     '''
-    __tablename__ = 'books'
+    __tablename__ = 'Book'
 
     id = Column(Integer, primary_key=True)
     name = Column(Unicode(1000), nullable=False)
@@ -28,6 +28,34 @@ class Book(Base):
             'name': self.name,
             'author': self.author,
         }
+
+
+class Genre(Base):
+    """
+    Schema of genre
+    """
+    __tablename__ = 'Genre'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(200), nullable=False)
+    updated_at = Column(DateTime,
+        nullable=False,
+        default=datetime.datetime.now())
+
+
+class BookGenreXref(Base):
+    """
+    Foreign key table for books and their tagged genre
+    """
+    __tablename__ = 'BookGenreXref'
+
+    id = Column(Integer, primary_key=True)
+    bookid = Column(Integer, ForeignKey('Book.id'), nullable=False)
+    genreid = Column(Integer, ForeignKey('Genre.id'), nullable=False)
+    updated_at = Column(DateTime,
+        nullable=False,
+        default=datetime.datetime.now())
+
 
 engine = create_engine('postgresql+psycopg2://libra:passw0rd@localhost/catalogue')
 Base.metadata.create_all(engine)
