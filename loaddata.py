3ca1921c30cb693e733f44aeb83b6d97938e8cb5
kerwei <kerwei84@gmail.com>
2020-01-05 19:26:30 +0800

    Load genre information to postgresql. Also add a method to guess the genre of a book and add the guesses to the associative BookGenreXref table

--- loaddata.py
+++ loaddata.py
@@ -4,7 +4,10 @@ import string
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Book
+from database_setup import Base, Book, Genre, BookGenreXref
+
+import langtools
+from langtools import CATEGORIES
 
 
 engine = create_engine('postgresql+psycopg2://libra:passw0rd@localhost/catalogue')
@@ -13,6 +16,7 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 ALPHANUM = set(string.ascii_letters).union(set([str(i) for i in range(10)]))
+THRESHOLD = 0.3
 
 # sample data
 mockdata = {}
@@ -33,7 +37,6 @@ def add_entries(data):
     Create book objects for each title - author
     """
     for row in data.values():
-        print(row['name'].split())
         if is_alphanum(row['name']):
             book = Book(name=row['name'], author=row['author'])
             yield book
@@ -45,6 +48,41 @@ def is_alphanum(sentence):
     return all([c in ALPHANUM for word in sentence.split() for c in word])
 
 
+def load_genre():
+    """
+    Adds all genre to postgre table
+    """
+    for cat in CATEGORIES:
+        session.add(Genre(name=cat))
+
+
 if __name__ == '__main__':
+    # Load mock data to base tables
+    load_genre()
     load_data(mockdata)
+
+    # Persist all mock data
     session.commit()
+
+    # Creates a dictionary of genre with the respective db primary keys
+    db_genre = session.query(Genre).all()
+    GENRE = {i.name: i.id for i in db_genre}
+
+    # Create foreign key relationship between book and genre
+    for k in mockdata.values():
+        book = session.query(Book).filter(Book.name == k['name'], Book.author == k['author']).limit(1).one_or_none()
+
+        if not book:
+            continue
+
+        features = langtools.extract_featureset(book.name)
+        genre_scoreboard = langtools.assign_genre(features)
+
+        tags = (GENRE[k] for k, v in genre_scoreboard.items() if v >= THRESHOLD)
+
+        for tag in tags:
+            book_genre = BookGenreXref(bookid=book.id, genreid=tag)
+            session.add(book_genre)
+
+    session.commit()
+
